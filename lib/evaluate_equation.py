c1abaf95c613e83d837cb347313a22aef7e799e4
Steven Greenup <sgreenup@alderbio.com>
2019-10-17 10:42:04 -0700

    fixed problem with mixed number calc with negative decimal values

--- lib/evaluate_equation.py
+++ lib/evaluate_equation.py
@@ -19,13 +19,19 @@ class EvaluateEquation(object):
 
     def to_mixed_number(self):
         fraction = self.to_fraction()
-        num = int(fraction.numerator)
-        den = int(fraction.denominator)
 
-        integral_part = num // den
-        fractional_part_num = num % den
+        num, den = (int(fraction.numerator), int(fraction.denominator))
+
+        whole_part = abs(num) // den
+
+        if num < 0:
+            whole_part = -whole_part
+
+        fractional_part_num = abs(num) % den
 
         if fractional_part_num > 0:
-            return ("%s_%s/%s" % (integral_part, fractional_part_num, den))
+
+
+            return ("%s_%s/%s" % (whole_part, fractional_part_num, den))
         else:
-            return ("%s" % integral_part)
+            return ("%s" % whole_part)
